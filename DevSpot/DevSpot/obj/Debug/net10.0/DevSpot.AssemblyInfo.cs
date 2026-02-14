95a8f0a6878dfda52a21546f162792f0c1a43d8f
circamanuel <circamanuel@users.noreply.github.com>
2026-02-14 22:04:21 +0100

    created JobPostingsController

--- DevSpot/DevSpot/obj/Debug/net10.0/DevSpot.AssemblyInfo.cs
+++ DevSpot/DevSpot/obj/Debug/net10.0/DevSpot.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevSpot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41399bf219530367c2024ce3e76b9f7a32a82414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f073a2e8cccb1a965dcfd67f0b051e2bb7880d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevSpot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevSpot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
