a6f2b6a85ef981b85d75da38ebeb6c5728acd407
circamanuel <circamanuel@users.noreply.github.com>
2026-02-24 22:23:07 +0100

    Feat(Project) add file upload project

--- FirstBlazorApp/FirstBlazorApp/Program.cs
+++ FirstBlazorApp/FirstBlazorApp/Program.cs
@@ -11,7 +11,9 @@ namespace FirstBlazorApp
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped
+                (sp => new HttpClient { 
+                    BaseAddress = new Uri("https://api.open-meteo.com/v1/") });
 
             await builder.Build().RunAsync();
         }
